b70faa2f67c82c592e7122eb669b1fbb00f8ece2
Aachman Garg <imaachman@gmail.com>
2025-03-27 18:46:39 +0530

    Final project fixes and enhancements
    - Create app directory if not already present in `exposeAppDirectoryFiles` method
    - Fix keyboard dismissal for book and borrower editors
    - Global form key was messing with the state, so we remove it and instead use `Form.of(context)` to validate the form input
    - Add dynamic bottom padding to book and borrower editors to adjust the bottom sheet when keyboard is opened
    - Update BookListTile UI to prevent render overflow issues

--- source-code/final-project/lib/features/books/screens/book_editor/book_editor.dart
+++ source-code/final-project/lib/features/books/screens/book_editor/book_editor.dart
@@ -16,103 +16,102 @@ import 'title_field.dart';
 class BookEditor extends StatelessWidget {
   final Book? book;
 
-  BookEditor({super.key, this.book});
-
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  const BookEditor({super.key, this.book});
 
   @override
   Widget build(BuildContext context) {
-    return DraggableScrollableSheet(
-      expand: false,
-      initialChildSize: 0.80,
-      builder: (context, scrollController) {
-        return SingleChildScrollView(
-          child: Form(
-            key: formKey,
-            child: Padding(
-              padding: const EdgeInsets.all(32),
-              child: LayoutBuilder(
-                builder: (context, constraints) {
-                  final double maxWidth = constraints.maxWidth;
-                  // Whether to build a column or a row. Depends on screen
-                  // size.
-                  final bool buildColumn = maxWidth < kSmallPhone / 3;
-                  // Gap between book cover and text fields.
-                  final double gap = 16;
+    return Padding(
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: DraggableScrollableSheet(
+        expand: false,
+        initialChildSize: 0.80,
+        builder: (context, scrollController) {
+          return SingleChildScrollView(
+            child: Form(
+              child: Padding(
+                padding: const EdgeInsets.all(32),
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    final double maxWidth = constraints.maxWidth;
+                    // Whether to build a column or a row. Depends on screen
+                    // size.
+                    final bool buildColumn = maxWidth < kSmallPhone / 3;
+                    // Gap between book cover and text fields.
+                    final double gap = 16;
 
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      RowColumnSwitch(
-                        columnWhen: buildColumn,
-                        crossAxisAlignment: buildColumn
-                            ? CrossAxisAlignment.center
-                            : CrossAxisAlignment.end,
-                        children: [
-                          SizedBox(
-                            width:
-                                buildColumn ? (maxWidth / 2) : (maxWidth / 3),
-                            child: EditableBookCover(book: book),
-                          ),
-                          SizedBox(
-                            width: buildColumn ? null : gap,
-                            height: buildColumn ? gap : null,
-                          ),
-                          SizedBox(
-                            width:
-                                buildColumn ? maxWidth : (maxWidth / 1.5 - gap),
-                            child: Column(
-                              children: [
-                                TitleField(book: book),
-                                SizedBox(height: 16),
-                                AuthorField(book: book),
-                              ],
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RowColumnSwitch(
+                          columnWhen: buildColumn,
+                          crossAxisAlignment: buildColumn
+                              ? CrossAxisAlignment.center
+                              : CrossAxisAlignment.end,
+                          children: [
+                            SizedBox(
+                              width:
+                                  buildColumn ? (maxWidth / 2) : (maxWidth / 3),
+                              child: EditableBookCover(book: book),
                             ),
-                          )
-                        ],
-                      ),
-                      SizedBox(height: 32),
-                      SummaryField(book: book),
-                      SizedBox(height: 32),
-                      RowColumnSwitch(
-                        columnWhen: buildColumn,
-                        crossAxisAlignment: buildColumn
-                            ? CrossAxisAlignment.center
-                            : CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            width:
-                                buildColumn ? maxWidth : (maxWidth - gap) / 2,
-                            child: GenreField(book: book),
-                          ),
-                          SizedBox(
-                            width: buildColumn ? null : gap,
-                            height: buildColumn ? gap : null,
-                          ),
-                          SizedBox(
-                            width:
-                                buildColumn ? maxWidth : (maxWidth - gap) / 2,
-                            child: ReleaseDateField(book: book),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 16),
-                      TotalCopiesField(book: book),
-                      SizedBox(height: 32),
-                      Center(
-                        child: BookAddUpdateButton(
-                          formKey: formKey,
-                          book: book,
+                            SizedBox(
+                              width: buildColumn ? null : gap,
+                              height: buildColumn ? gap : null,
+                            ),
+                            SizedBox(
+                              width: buildColumn
+                                  ? maxWidth
+                                  : (maxWidth / 1.5 - gap),
+                              child: Column(
+                                children: [
+                                  TitleField(book: book),
+                                  SizedBox(height: 16),
+                                  AuthorField(book: book),
+                                ],
+                              ),
+                            )
+                          ],
                         ),
-                      ),
-                    ],
-                  );
-                },
+                        SizedBox(height: 32),
+                        SummaryField(book: book),
+                        SizedBox(height: 32),
+                        RowColumnSwitch(
+                          columnWhen: buildColumn,
+                          crossAxisAlignment: buildColumn
+                              ? CrossAxisAlignment.center
+                              : CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width:
+                                  buildColumn ? maxWidth : (maxWidth - gap) / 2,
+                              child: GenreField(book: book),
+                            ),
+                            SizedBox(
+                              width: buildColumn ? null : gap,
+                              height: buildColumn ? gap : null,
+                            ),
+                            SizedBox(
+                              width:
+                                  buildColumn ? maxWidth : (maxWidth - gap) / 2,
+                              child: ReleaseDateField(book: book),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        TotalCopiesField(book: book),
+                        SizedBox(height: 32),
+                        Center(
+                          child: BookAddUpdateButton(book: book),
+                        ),
+                      ],
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
--- source-code/final-project/lib/features/borrowers/screens/borrower_editor/borrower_editor.dart
+++ source-code/final-project/lib/features/borrowers/screens/borrower_editor/borrower_editor.dart
@@ -13,101 +13,102 @@ import 'package:libertad/widgets/row_column_switch.dart';
 class BorrowerEditor extends StatelessWidget {
   final Borrower? borrower;
 
-  BorrowerEditor({super.key, this.borrower});
-
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  const BorrowerEditor({super.key, this.borrower});
 
   @override
   Widget build(BuildContext context) {
-    return DraggableScrollableSheet(
-      expand: false,
-      initialChildSize: 0.60,
-      builder: (context, scrollController) {
-        return SingleChildScrollView(
-          child: Form(
-            key: formKey,
-            child: Padding(
-              padding: const EdgeInsets.all(32),
-              child: LayoutBuilder(
-                builder: (context, constraints) {
-                  final double maxWidth = constraints.maxWidth;
-                  // Whether to build a column or a row. Depends on screen
-                  // size.
-                  final bool buildColumn = maxWidth < kSmallPhone / 3;
-                  // Gap between book cover and text fields.
-                  final double gap = 20;
+    return Padding(
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: DraggableScrollableSheet(
+        expand: false,
+        initialChildSize: 0.60,
+        builder: (context, scrollController) {
+          return SingleChildScrollView(
+            child: Form(
+              child: Padding(
+                padding: const EdgeInsets.all(32),
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    final double maxWidth = constraints.maxWidth;
+                    // Whether to build a column or a row. Depends on screen
+                    // size.
+                    final bool buildColumn = maxWidth < kSmallPhone / 3;
+                    // Gap between book cover and text fields.
+                    final double gap = 20;
 
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      RowColumnSwitch(
-                        columnWhen: buildColumn,
-                        crossAxisAlignment: buildColumn
-                            ? CrossAxisAlignment.center
-                            : CrossAxisAlignment.end,
-                        children: [
-                          SizedBox(
-                            width:
-                                buildColumn ? (maxWidth / 2) : (maxWidth / 3),
-                            child: EditableProfilePicture(borrower: borrower),
-                          ),
-                          SizedBox(
-                            width: buildColumn ? null : gap,
-                            height: buildColumn ? gap : null,
-                          ),
-                          SizedBox(
-                            width:
-                                buildColumn ? maxWidth : (maxWidth / 1.5 - gap),
-                            child: Column(
-                              children: [
-                                NameField(borrower: borrower),
-                                SizedBox(height: 16),
-                                ContactField(borrower: borrower),
-                              ],
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RowColumnSwitch(
+                          columnWhen: buildColumn,
+                          crossAxisAlignment: buildColumn
+                              ? CrossAxisAlignment.center
+                              : CrossAxisAlignment.end,
+                          children: [
+                            SizedBox(
+                              width:
+                                  buildColumn ? (maxWidth / 2) : (maxWidth / 3),
+                              child: EditableProfilePicture(borrower: borrower),
                             ),
-                          )
-                        ],
-                      ),
-                      SizedBox(height: 32),
-                      // SummaryField(book: book),
-                      // SizedBox(height: 32),
-                      RowColumnSwitch(
-                        columnWhen: buildColumn,
-                        crossAxisAlignment: buildColumn
-                            ? CrossAxisAlignment.center
-                            : CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            width:
-                                buildColumn ? maxWidth : (maxWidth - gap) / 2,
-                            child: MembershipStartDateField(borrower: borrower),
-                          ),
-                          SizedBox(
-                            width: buildColumn ? null : gap,
-                            height: buildColumn ? gap : null,
-                          ),
-                          SizedBox(
-                            width:
-                                buildColumn ? maxWidth : (maxWidth - gap) / 2,
-                            child: MembershipDurationField(borrower: borrower),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 32),
-                      Center(
-                        child: BorrowerAddUpdateButton(
-                          formKey: formKey,
-                          borrower: borrower,
+                            SizedBox(
+                              width: buildColumn ? null : gap,
+                              height: buildColumn ? gap : null,
+                            ),
+                            SizedBox(
+                              width: buildColumn
+                                  ? maxWidth
+                                  : (maxWidth / 1.5 - gap),
+                              child: Column(
+                                children: [
+                                  NameField(borrower: borrower),
+                                  SizedBox(height: 16),
+                                  ContactField(borrower: borrower),
+                                ],
+                              ),
+                            )
+                          ],
                         ),
-                      ),
-                    ],
-                  );
-                },
+                        SizedBox(height: 32),
+                        // SummaryField(book: book),
+                        // SizedBox(height: 32),
+                        RowColumnSwitch(
+                          columnWhen: buildColumn,
+                          crossAxisAlignment: buildColumn
+                              ? CrossAxisAlignment.center
+                              : CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width:
+                                  buildColumn ? maxWidth : (maxWidth - gap) / 2,
+                              child:
+                                  MembershipStartDateField(borrower: borrower),
+                            ),
+                            SizedBox(
+                              width: buildColumn ? null : gap,
+                              height: buildColumn ? gap : null,
+                            ),
+                            SizedBox(
+                              width:
+                                  buildColumn ? maxWidth : (maxWidth - gap) / 2,
+                              child:
+                                  MembershipDurationField(borrower: borrower),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 32),
+                        Center(
+                          child: BorrowerAddUpdateButton(borrower: borrower),
+                        ),
+                      ],
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
